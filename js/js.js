512ec812fd0a04d6186b82ec1009684bee2f6d18
SamMorgan <sam@svmorgan.com>
2015-09-30 14:02:11 +1000

    Uriahs styling changes

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 // @codekit-prepend "/lib/jquery.vticker.min.js";
 
+
 jQuery(document).ready(function($){
 
 	var $win = $(window),
@@ -43,7 +44,7 @@ jQuery(document).ready(function($){
 			}
 		});
 		$container.height(newHeight);
-		$('.container_wrap').addClass('ready');
+		//$('.container_wrap').addClass('ready');
 	}
 
 	// drag function //
@@ -199,5 +200,168 @@ jQuery(document).ready(function($){
 	           window.open(this.href, '_blank');
 	       });
 	   }
-	});	
+	});
+
+	// Draw //
+  var ctx,x,y;
+  function newCanvas(){
+
+  var winWidth = $(document).width();
+  var winHeight = $(document).height();
+  //var winHeight2 = $(window).height();
+
+
+  var canvasWidth = winWidth;
+  var canvasHeight = winHeight;
+
+  var canvasDiv = document.getElementById('canvasdiv');
+  var canvas = document.createElement('canvas');
+  canvas.setAttribute('width', canvasWidth);
+  canvas.setAttribute('height', canvasHeight);
+  canvas.setAttribute('id', 'canvas');
+  canvasDiv.appendChild(canvas);
+  if(typeof G_vmlCanvasManager !== 'undefined') {
+    canvas = G_vmlCanvasManager.initElement(canvas);
+  }
+  //var context = canvas.getContext("2d");
+    
+    // setup canvas
+  ctx = document.getElementById("canvas").getContext("2d");
+  ctx.strokeStyle = "#000";
+  ctx.lineJoin = "round";
+  ctx.lineWidth = 5;  
+  
+  // setup to trigger drawing on mouse or touch
+  $("#canvas").drawTouch();
+  $("#canvas").drawPointer();
+  $("#canvas").drawMouse();
+}
+// prototype to start drawing on touch using canvas moveTo and lineTo
+$.fn.drawTouch = function() {
+  var start = function(e) {
+    e = e.originalEvent;
+    ctx.beginPath();
+    x = e.changedTouches[0].pageX;
+    y = e.changedTouches[0].pageY;
+    ctx.moveTo(x,y);
+  };
+  var move = function(e) {
+    e.preventDefault();
+        e = e.originalEvent;
+    x = e.changedTouches[0].pageX;
+    y = e.changedTouches[0].pageY;
+    ctx.lineTo(x,y);
+    ctx.stroke();
+  };
+  var stop = function() {
+    $('#canvas').css('pointer-events','none');
+  }; 
+  $(document).on("touchstart",function(){
+  	if(event.target.id === 'dragdrop_container'){
+  		$('#canvas').css('pointer-events','auto');
+  		start();
+  	}  	
+  });   
+  //$(this).on("touchstart", start);
+  $(this).on("touchmove", move);
+  $(this).on("touchend", function(){ stop();});  
+}; 
+    
+// prototype to start drawing on pointer(microsoft ie) using canvas moveTo and lineTo
+$.fn.drawPointer = function() {
+  var start = function(e) {
+    e = e.originalEvent;
+    ctx.beginPath();
+    x = e.pageX;
+    y = e.pageY-44;
+    ctx.moveTo(x,y);
+  };
+  var move = function(e) {
+    e.preventDefault();
+        e = e.originalEvent;
+    x = e.pageX;
+    y = e.pageY-44;
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    };
+  $(this).on("MSPointerDown", start);
+  $(this).on("MSPointerMove", move);
+};        
+// prototype to start drawing on mouse using canvas moveTo and lineTo
+$.fn.drawMouse = function() {
+  var clicked = 0;
+  var start = function(e) {
+    clicked = 1;
+    ctx.beginPath();
+    x = e.pageX;
+    y = e.pageY;
+    ctx.moveTo(x,y);
+  };
+  var move = function(e) {
+    if(clicked){
+      x = e.pageX;
+      y = e.pageY;
+      ctx.lineTo(x,y);
+      ctx.stroke();
+    }
+  };
+  var stop = function() {
+    clicked = 0;
+    $('#canvas').css('pointer-events','none');
+  };
+  //$(this).on("mousedown",start); 	
+  
+  $(document).on("mousedown",function(){
+  	if(event.target.id === 'dragdrop_container'){
+  		$('#canvas').css('pointer-events','auto');
+  		start();
+  	}  	
+  });
+  
+  $(this).on("mousemove", move);
+  $(window).on("mouseup", stop);
+  //$(window).on("touchend", stop);
+   //var winWidth = $(document).width();
+    //var winHeight = $(document).height();
+   //var winHeight2 = $(window).height();
+
+}; 
+
+  var gm = (function() {
+
+      // var winWidth = $(document).width();
+      // var winHeight = $(document).height();
+      // var winHeight2 = $(window).height();
+
+
+    // setup a new canvas for drawing wait for device init
+      setTimeout(function(){
+       newCanvas();
+      }, 1000);
+
+  });
+
+  $(window).load(gm);	
+	$(window).load(function(){
+		$('.container_wrap').addClass('ready');
+		function test(){
+			var canvas  = document.getElementById("canvas");
+			var dataURL = canvas.toDataURL();
+
+			$.ajax({
+			  type: "POST",
+			  url: "upload.php",
+			  data: { 
+			     imgBase64: dataURL
+			  }
+			});
+		}
+
+		$('.save').click(function() {
+		   test();
+		   $(".saved").show();
+		    setTimeout(function() { $(".saved").hide(); }, 2000);
+		});
+	});		
+		
 });	
